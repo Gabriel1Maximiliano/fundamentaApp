81676234b3c2a684f631a2bcf471c1c390e26c9b
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2022-12-23 22:51:03 -0300

    uso de computed prperties

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,4 +1,16 @@
-
+<template>
+    <h1>Contador sin computed properties</h1>
+       <p>{{count}} <sup>2</sup> = {{ getSquareValue() }}</p>
+       <p>{{count}} <sup>2</sup> = {{ getSquareValue() }}</p>
+       <p>{{count}} <sup>2</sup> = {{ getSquareValue() }}</p>
+       <p>{{count}} <sup>2</sup> = {{ getSquareValue() }}</p>
+       <h1>Contador con computed properties</h1>
+       <p>{{count}} <sup>2</sup> = {{ computedSquare }}</p>
+       <p>{{count}} <sup>2</sup> = {{ computedSquare }}</p>
+       <p>{{count}} <sup>2</sup> = {{ computedSquare }}</p>
+       <p>{{count}} <sup>2</sup> = {{ computedSquare }}</p>
+     <button @click="count++">Count is: {{ count }}</button>
+   </template>
 
 <script>
 export default {
@@ -7,15 +19,24 @@ export default {
     return {
       count: 0
     }
-  }
+},
+methods:{
+ getSquareValue() {// esta se dipara cada evez que se llama a la fncion 
+    console.log('me dipare soy getSquareValue ')
+    return this.count*this.count
+ }
+},
+computed:{// las propiedades computadas se guardan en cache por eso se dispara solo una vez
+    // esta caracteristica ayuda con los procesos pesados de la app
+    computedSquare(){
+        console.log('me dispare soy computedSquare')
+        return this.count*this.count
+    }
+}
 }
 </script>
 
-<template>
- <h1>Contador</h1>
-    <p>{{count}} <sup>2</sup> = {{ count*count }}</p>
-  <button @click="count++">Count is: {{ count }}</button>
-</template>
+
 
 <style>
 
