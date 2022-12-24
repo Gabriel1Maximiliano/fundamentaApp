eb3b3bb1865f870b7f3c460fac51058a4ffa47b9
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2022-12-23 22:29:24 -0300

    first commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,22 @@
+
+
+<script>
+export default {
+    name:'Patito feo',
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<template>
+ <h1>Contador</h1>
+    <p>{{count}}</p>
+  <button @click="count++">Count is: {{ count }}</button>
+</template>
+
+<style>
+
+</style>
